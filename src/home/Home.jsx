c4dd6ca7e052d2a4c4ee2f1502bd2ca7562ea19f
praveen-neshvi <71161341+praveen-neshvi@users.noreply.github.com>
2021-09-17 02:41:13 +0530

    Slider Completed

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -3,12 +3,15 @@ import {AcUnit} from '@material-ui/icons'
 import './home.scss'
 import Navbar from '../components/navbar/Navbar'
 import Featured from '../components/featured/Featured'
+import List from '../components/list/List';
 
 const Home = () => {
     return (
         <div className="home">
            <Navbar />
-           <Featured type="series"/>
+           <Featured type=""/>
+           <List />
+           <List />
         </div>
     )
 }
